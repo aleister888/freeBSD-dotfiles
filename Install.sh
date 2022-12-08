dcfbf4f993c3604c867aae1ddfb6e9a1af01cf7f
aleister888 <pacoe1000@protonmail.com>
2022-12-08 13:22:18 +0100

    modificados:     Install.sh

--- Install.sh
+++ Install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir $HOME/.local/scripts; stow --target=${HOME}/.local/scripts/ scripts/
 mkdir $HOME/.config; stow --target=${HOME}/.config/ .config/
+ln -s ~/.dotfiles/.zshenv ~/
+ln -s ~/.dotfiles/.profile ~/
